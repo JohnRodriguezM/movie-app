17d14f271a94af1306cd2fc11df4783d1624adc3
john jairo rodriguez medina <johnjairorodriguez384@gmail.com>
2022-05-27 23:16:16 -0500

    entorno de trabajo listo

--- js/main-peticiones.js
+++ js/main-peticiones.js
@@ -1,15 +1,11 @@
-'use strict';
+"use strict";
 
 // ? para ponerlos dentro de los headers
-console.log(API_KEY, nombre, hola(),KEY)
+console.log(API_KEY, KEY);
 
 //? para poner como query parameter
 
-
-
-
-
-const getData = async () => {}
-const postData = async () => {}
-const putData = async () => {}
-const deleteData = async () => {}
+const getData = async () => {};
+const postData = async () => {};
+const putData = async () => {};
+const deleteData = async () => {};
